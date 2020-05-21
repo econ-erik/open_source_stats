1115ab4b305989b01f479b4987de5985e1434c23
Erik <56318768+econ-erik@users.noreply.github.com>
2020-05-21 10:23:55 -0400

    Updates to reflect initial and continuing claims

--- pdf_tools_ui_claims.R
+++ pdf_tools_ui_claims.R
@@ -9,7 +9,8 @@ library(httr)
 library(janitor)
 library(openintro)
 # read in PDF
-ui_text <- pdf_text(paste(data_path,"ui_claims_apr11.pdf",sep="")) # fill in most recent pdf
+# if reading straight from the DOL website switch the argument in pdf_text() to "https://www.dol.gov/ui/data.pdf" 
+ui_text <- pdf_text(paste(data_path,"ui_claims_may09.pdf",sep="")) # fill in most recent pdf
 # subset to State UI Claims Page
 ui_claims <- ui_text[[5]]
 # split into a table based on row separator
@@ -22,16 +23,35 @@ ui_claims<- read.csv(ui_claims, sep="|", header = FALSE)
 # clean up column names
 names(ui_claims) <- as.character(unlist(ui_claims[2,]))
 # filter out additional rows and remove insured columns 
-ui_claims <- ui_claims[-c(1:2,57:62),-c(5:7)] # may need to adjust rows
-ui_claims <- janitor::clean_names(ui_claims)
-names(ui_claims)[1] <- "state"
+ui_claims_1 <- ui_claims[-c(1:2,57:62),-c(5:7)] # may need to adjust rows
+ui_claims_1 <- janitor::clean_names(ui_claims_1)
+names(ui_claims_1)[1] <- "state"
 # convert columns to numeric
-ui_claims$advance <- as.numeric(gsub(",","",ui_claims$advance))
-ui_claims$prior_wk <- as.numeric(gsub(",","",ui_claims$prior_wk))
-ui_claims$change <- as.numeric(gsub(",","",ui_claims$change))
-# add a resort and state abbreviations 
-ui_claims$abbr[!ui_claims$state %in% c("US Total","Virgin Islands","")] <- state2abbr(ui_claims$state)
-ui_claims <- ui_claims %>%
-  select(abbr,state,advance,prior_wk,change)
+ui_claims_1$advance <- as.numeric(gsub(",","",ui_claims_1$advance))
+ui_claims_1$prior_wk <- as.numeric(gsub(",","",ui_claims_1$prior_wk))
+ui_claims_1$change <- as.numeric(gsub(",","",ui_claims_1$change))
+# add a resort and state abbreviations and delete Puerto Rico and VI
+ui_claims_1 <- ui_claims_1[!ui_claims_1$state %in% c("Virgin Islands","Puerto Rico"),]
+ui_claims_1$abbr[!ui_claims_1$state %in% c("US Total")] <- state2abbr(ui_claims_1$state)
+ui_claims_1 <- ui_claims_1 %>%
+  select(abbr,state,advance,prior_wk,change) %>%
+  arrange(abbr)
 # write to csv
-write_csv(ui_claims,paste(data_path,"ui_claims",today(),".csv",sep=""), col_names = TRUE)
+write_csv(ui_claims_1,paste(data_path,"ui_claims_",today(),".csv",sep=""), col_names = TRUE)
+# grab continuing claims
+# filter out additional rows and remove insured columns 
+ui_claims_2 <- ui_claims[-c(1:2,57:62),-c(2:4)] # may need to adjust rows
+ui_claims_2 <- janitor::clean_names(ui_claims_2)
+names(ui_claims_2)[1] <- "state"
+# convert columns to numeric
+ui_claims_2$advance <- as.numeric(gsub(",","",ui_claims_2$advance))
+ui_claims_2$prior_wk <- as.numeric(gsub(",","",ui_claims_2$prior_wk))
+ui_claims_2$change <- as.numeric(gsub(",","",ui_claims_2$change))
+# add a resort and state abbreviations and delete Puerto Rico and VI
+ui_claims_2 <- ui_claims_2[!ui_claims_2$state %in% c("Virgin Islands","Puerto Rico"),]
+ui_claims_2$abbr[!ui_claims_2$state %in% c("US Total")] <- state2abbr(ui_claims_2$state)
+ui_claims_2 <- ui_claims_2 %>%
+  select(abbr,state,advance,prior_wk,change) %>%
+  arrange(abbr)
+# write to csv
+write_csv(ui_claims_2,paste(data_path,"ui_continue_",today(),".csv",sep=""), col_names = TRUE)
